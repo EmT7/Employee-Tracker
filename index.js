0d4666a9db2822e7528025657ab2a00928d9ecb3
Miranda <88161424+MirandaT77@users.noreply.github.com>
2021-10-23 14:24:52 -0700

    adding menu

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
+
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
-const connection = require("connection");
+const database = require("./db")
 const promisemysqyl = require("promise-mysql");
 
 
+// Establish Connection
 const connection = {
     host: "localhost",
     port:3003,
     user:"root",
     password:"bootcamp",
-    database:""
+    database:"cms"
 }
 
+const connection = mysql.createConnection(connection
+    );
 connection.connect((err) => {
     if (err) throw err;
-    inquirer.prompts();
+    console.log("Employee Tracker");
 });
+
+// Create main menu
+
+function mainMenu() {
+//prompting user 
+inquirer
+.prompt({
+    name:"action",
+    type:"list",
+    message:"Menu",
+    choices: [
+        "View all employees",
+        "Add employee",
+        "Update employee",
+        "Delete employee",
+        "View department budget"
+    ]
+})
+}
 
 module.exports = connection;
