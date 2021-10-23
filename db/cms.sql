b22ecae7af2035ec913e43004e0d990b1c6cf562
Miranda <88161424+MirandaT77@users.noreply.github.com>
2021-10-23 13:15:57 -0700

    added role table

--- db/cms.sql
+++ db/cms.sql
@@ -6,3 +6,12 @@ CREATE TABLE department (
   id INTEGER NOT NULL auto_increment PRIMARY KEY,
   name VARCHAR(50)
 );
+
+CREATE TABLE role (
+  id INTEGER NOT NULL auto_increment PRIMARY KEY,
+  title VARCHAR(50),
+  salary DECIMAL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
