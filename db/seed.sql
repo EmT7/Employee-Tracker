486ace0189abcca95232858a6645e4caf5bfe913
Miranda <88161424+MirandaT77@users.noreply.github.com>
2021-10-23 13:28:23 -0700

    inserting cms data

---
+++ db/seed.sql
@@ -0,0 +1,22 @@
+USE cms;
+
+INSERT into department (name, id) VALUES ("Marketing");
+INSERT into department (name, ) VALUES ("");
+INSERT into department (name) VALUES ("");
+INSERT into department (name) VALUES ("");
+INSERT into department (name) VALUES ("");
+INSERT into department (name) VALUES ("");
+
+INSERT into role (id, title, salary, department_id) VALUES ("");
+INSERT into role (id, title, salary, department_id) VALUES ("");
+INSERT into role (id, title, salary, department_id) VALUES ("");
+INSERT into role (id, title, salary, department_id) VALUES ("");
+INSERT into role (id, title, salary, department_id) VALUES ("");
+INSERT into role (id, title, salary, department_id) VALUES ("");
+
+INSERT into employee (id, first_name, last_name, role_id, manager_id) VALUES ("");
+INSERT into employee (id, first_name, last_name, role_id, manager_id) VALUES ("");
+INSERT into employee (id, first_name, last_name, role_id, manager_id) VALUES ("");
+INSERT into employee (id, first_name, last_name, role_id, manager_id) VALUES ("");
+INSERT into employee (id, first_name, last_name, role_id, manager_id) VALUES ("");
+INSERT into employee (id, first_name, last_name, role_id, manager_id) VALUES ("");
